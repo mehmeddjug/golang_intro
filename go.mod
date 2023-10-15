9c794afad94dde41e2c2803b188cf815945b95cc
Djugson <mehmed.djug@outlook.com>
2023-10-15 13:20:24 +0200

    Initial commit - adding dummy package with init function.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mehmeddjug/golang_intro
+
+go 1.21.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+
+    _ "github.com/mehmeddjug/golang_intro/dummy"
+)
+
+
+func main() {
+    fmt.Println("This is a main func!")
+    fmt.Println("Hello World!")
+}
