530db22d7cf4503867fedb11ef85f3e93987cb57
mehmeddjug <mehmed.djug@outlook.com>
2023-10-17 22:52:40 +0200

    Add function with arguments as array and pointers to array.

--- main.go
+++ main.go
@@ -8,7 +8,14 @@ import (
 )
 
 func main() {
-	fmt.Println("This is a main func!")
+	fmt.Println("--------------------------------------------------")
 	datastructs.Decleration()
+	fmt.Println("--------------------------------------------------")
+	arr := [3]string{"Gold", "Silver", "Bronze"}
+	datastructs.Array(arr)
+	fmt.Println("--------------------------------------------------")
+	ptrArr := [3]*string{new(string), new(string), new(string)}
+	datastructs.PtrArray(ptrArr)
+	fmt.Println("--------------------------------------------------")
 	fmt.Println("Hello World!")
 }
