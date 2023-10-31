70765c873db9a4dfe1ac993860fdee6e09db2085
mehmeddjug <mehmed.djug@outlook.com>
2023-10-31 23:23:36 +0100

    Add Some numbers to map.

--- datastructs/mapds/mapds.go
+++ datastructs/mapds/mapds.go
@@ -11,4 +11,15 @@ func Decleration() {
 	// Create and initialize the map with 2 key/value of type string pairs.
 	mapDictTwo := map[string]string{"One": "One-Two", "Three": "Three-Four"}
 	fmt.Println(mapDictTwo)
+
+	// Create a map using a slice of strings as the value.
+	mapDictThree := map[int][]string{}
+	fmt.Println(mapDictThree)
+
+	// Create an empty map.
+	emptyMap := map[string]string{}
+
+	// Add Some numbers to emptyMap.
+	emptyMap["Some"] = "numbers"
+	fmt.Println(emptyMap)
 }
