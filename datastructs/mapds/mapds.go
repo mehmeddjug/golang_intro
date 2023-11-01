e61ab23aff740d60aa53a0d0f72549c049918551
mehmeddjug <mehmed.djug@outlook.com>
2023-11-01 15:24:10 +0100

    Add maps decleration exercises

--- datastructs/mapds/mapds.go
+++ datastructs/mapds/mapds.go
@@ -16,10 +16,34 @@ func Decleration() {
 	mapDictThree := map[int][]string{}
 	fmt.Println(mapDictThree)
 
+	// Declering a nil map.
+	var emptyMapNil map[string]string
+	fmt.Println(emptyMapNil)
+
 	// Create an empty map.
 	emptyMap := map[string]string{}
 
 	// Add Some numbers to emptyMap.
 	emptyMap["Some"] = "numbers"
+	fmt.Println(emptyMap)
+
+	// Read value of the key "Some".
+	value1, exists := emptyMap["Some"]
+
+	// Check if the key exists?
+	if exists {
+		fmt.Println(value1)
+	}
+
+	// Read value of the key "Some".
+	value2 := emptyMap["Some"]
+
+	// Check if the key exists?
+	if value2 != "" {
+		fmt.Println(value2)
+	}
+
+	// Delete the key/value pair from the map.
+	delete(emptyMap, "Some")
 	fmt.Println(emptyMap)
 }
