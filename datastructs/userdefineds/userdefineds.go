78b5dee6bd8d185653b07e87d2ca526abcb2bb64
mehmeddjug <mehmed.djug@outlook.com>
2023-11-18 16:21:27 +0100

    Created function to use the dummy interface

--- datastructs/userdefineds/userdefineds.go
+++ datastructs/userdefineds/userdefineds.go
@@ -2,7 +2,13 @@ package userdefineds
 
 import "fmt"
 
-type user struct {
+type userOne struct {
+	name string
+	pass bool
+	age  int16
+}
+
+type userTwo struct {
 	name string
 	pass bool
 	age  int16
@@ -10,16 +16,24 @@ type user struct {
 
 type timeout int32
 
+type helloer interface {
+	hello()
+}
+
+func sayHello(h helloer) {
+	h.hello()
+}
+
 func Decleration() {
 	// user defined type user (struct)
-	var someOne user
+	var someOne userOne
 
-	someTwo := user{
+	someTwo := userOne{
 		name: "James",
 		pass: true,
 		age:  25,
 	}
-	someThree := user{"John", false, 35}
+	someThree := userOne{"John", false, 35}
 
 	fmt.Println(someOne)
 	fmt.Println(someTwo)
@@ -29,6 +43,7 @@ func Decleration() {
 	delay := timeout(30)
 	fmt.Println("Delay:", delay)
 
+	someThree.hello()
 	someTwo.hello()
 	someThree.updateName("Michael")
 	someThree.hello()
@@ -36,12 +51,38 @@ func Decleration() {
 
 // Method decleration -> func (type) name
 // Call by value receiver
-func (v user) hello() {
+func (v userOne) hello() {
 	fmt.Printf("Hello %s, I'm %d\n", v.name, v.age)
 }
 
 // Method decleration -> func (type) name
 // Call by pointer receiver
-func (v *user) updateName(name string) {
+func (v *userOne) updateName(name string) {
 	v.name = name
+}
+
+// Method decleration -> func (type) name
+// Call by value receiver
+func (v userTwo) hello() {
+	fmt.Printf("Hello %s\n", v.name)
+}
+
+// Method decleration -> func (type) name
+// Call by pointer receiver
+func (v *userTwo) updateName(name string) {
+	v.name = name
+}
+
+func DeclerationWithInterface() {
+	someOne := userOne{"John", false, 35}
+
+	someTwo := userTwo{
+		name: "James",
+		pass: true,
+		age:  25,
+	}
+	fmt.Printf("Interface:\n")
+	sayHello(someOne)
+	sayHello(someTwo)
+	fmt.Printf("End of functions.\n")
 }
